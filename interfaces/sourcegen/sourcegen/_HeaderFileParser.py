7e3c82ee09d95b082fd3dfe56d8509bda2547053
Ingmar Schoegl <ischoegl@lsu.edu>
2024-08-19 10:15:21 -0500

    [sourcegen] Extend Func definition

--- interfaces/sourcegen/sourcegen/_HeaderFileParser.py
+++ interfaces/sourcegen/sourcegen/_HeaderFileParser.py
@@ -8,7 +8,7 @@ import logging
 import re
 from typing import List
 
-from ._dataclasses import HeaderFile, Func, Recipe, Param
+from ._dataclasses import HeaderFile, Func, Recipe
 from ._helpers import read_config
 
 
@@ -20,25 +20,9 @@ _data_path = Path(__file__).parent.joinpath("_data").resolve()
 
 class HeaderFileParser:
 
-    @staticmethod
-    def _parse_param(c_param: str) -> Param:
-        parts = c_param.strip().rsplit(" ", 1)
-        if len(parts) == 2:
-            return Param(*parts)
-
     @classmethod
     def _parse_func(cls, func_comment: tuple[str, str]) -> Func:
-        c_func, annotations = func_comment
-        lparen = c_func.index("(")
-        rparen = c_func.index(")")
-        front = c_func[0:lparen].split()
-
-        params = list(map(cls._parse_param,
-            filter(None, c_func[(lparen + 1):rparen].split(","))))
-
-        ret_type = front[-2]
-        name = front[-1]
-        return Func(annotations, ret_type, name, params)
+        return Func.from_str(*func_comment)
 
     def __init__(self, path: Path, ignore_funcs: List[str] = None):
         self._path = path
@@ -99,7 +83,7 @@ class HeaderFileParser:
                         # new function: flush buffers
                         pairs.append((function, "\n".join(comments)))
                         comments = []
-                        function = m.group("func")
+                        function = m.group("func").replace("CANTERA_CAPI ", "")
                     elif m.group("head"):
                         # new heading comment: flush buffers
                         pairs.append((function, "\n".join(comments)))
@@ -122,7 +106,7 @@ class HeaderFileParser:
                         comments.append(m.group("head"))
                     elif m.group("func"):
                         # new function: buffer
-                        function = m.group("func")
+                        function = m.group("func").replace("CANTERA_CAPI ", "")
             return pairs
 
         c_functions = parse_with_doxygen(ct)
